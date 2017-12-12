0c806c81384635199f8b1e3892155df53ed6af6f
Marcos <marcos.flores.smart.lines@gmail.com>
2017-12-11 20:57:29 -0600

    Modificacion en la vista de Administracion

--- view/administracion/contenedor.php
+++ view/administracion/contenedor.php
@@ -1,19 +1,28 @@
 <br>
 <div class="margen2">
-  <div class="left_nav">
-    <div class="btn-group-vertical" role="group">
-      <a href="#" class="btn btn-default btn-sm">Embudo</a>
-      <a href="#" class="btn btn-default btn-sm">Usuarios y Permisos</a>
-      <a href="#" class="btn btn-default btn-sm">Otra opcion</a>
-      <a href="#" class="btn btn-default btn-sm">Otra opcion</a>
+<section class="main row">
+  <aside class="col-xs-3 col-sm-4 col-md-3 col-lg-2">
+    <div class="left_nav">
+      <div class="btn-group-vertical" role="group">
+        <a href="#" class="btn btn-default btn-sm">Embudo</a>
+        <a href="#" class="btn btn-default btn-sm">Usuarios y Permisos</a>
+        <a href="#" class="btn btn-default btn-sm">Otra opcion</a>
+        <a href="#" class="btn btn-default btn-sm">Otra opcion</a>
+      </div>
     </div>
-  </div>
-  <div class="container">
-    <div class="row">
-      <h2>Personalizar Etapas</h2>
+  </aside>
+  <br>
+  <article class="col-xs-9 col-sm-8 col-md-9 col-lg-10">
+    <div class="containerperso">
+            <div class="row">
+        <h2>Personalizar Etapas</h2>
+      </div>
+      <div class="row">
+        <a href="#" class="btn btn-success btn-sm">Añadir embudo nuevo</a>
+      </div>  
+      
     </div>
-    <div class="row">
-      <a href="#" class="btn btn-success btn-sm">Añadir embudo nuevo</a>
-    </div>  
-  </div>
+
+  </article>
+</section>
 </div>
