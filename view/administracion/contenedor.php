13ee3f4388e8cb946fa1ce4d3d86e2e185d3387d
Marcos <marcos.flores.smart.lines@gmail.com>
2017-12-12 00:28:35 -0600

    Nueva Modificacion en la vista de Administracion

--- view/administracion/contenedor.php
+++ view/administracion/contenedor.php
@@ -1,28 +1,40 @@
 <br>
 <div class="margen2">
-<section class="main row">
-  <aside class="col-xs-3 col-sm-4 col-md-3 col-lg-2">
-    <div class="left_nav">
-      <div class="btn-group-vertical" role="group">
-        <a href="#" class="btn btn-default btn-sm">Embudo</a>
-        <a href="#" class="btn btn-default btn-sm">Usuarios y Permisos</a>
-        <a href="#" class="btn btn-default btn-sm">Otra opcion</a>
-        <a href="#" class="btn btn-default btn-sm">Otra opcion</a>
+  <section class="main row">
+    <aside class="col-xs-3 col-sm-4 col-md-3 col-lg-2">
+      <div class="left_nav">
+        <div class="btn-group-vertical" role="group">
+          <a href="#" class="btn btn-default btn-sm">Embudo</a>
+          <a href="#" class="btn btn-default btn-sm">Usuarios y Permisos</a>
+          <a href="#" class="btn btn-default btn-sm">Otra opcion</a>
+          <a href="#" class="btn btn-default btn-sm">Otra opcion</a>
+          <a href="#" class="btn btn-default btn-sm">Usuarios y Permisos</a>
+          <a href="#" class="btn btn-default btn-sm">Otra opcion</a>
+          <a href="#" class="btn btn-default btn-sm">Otra opcion</a>
+        </div>
       </div>
-    </div>
-  </aside>
-  <br>
-  <article class="col-xs-9 col-sm-8 col-md-9 col-lg-10">
-    <div class="containerperso">
-            <div class="row">
-        <h2>Personalizar Etapas</h2>
+    </aside>
+    <article class="col-xs-9 col-sm-8 col-md-9 col-lg-10">
+      <div class="containerperso">
+        <div class="row">
+          <div class="col-xs-6 col-lg-6">
+            <h3>Personalizar Etapas</h3>
+          </div>
+          <div class="col-xs-6 col-lg-6">
+            <br>
+            <a href="#" class="btn btn-success btn-xs">Añadir embudo nuevo</a>
+          </div>
+        </div>
+        <br>
+        <div class="row">
+          <div class="col-xs-12 col-lg-12">
+            <ul class="nav nav-tabs">
+              <li role="presentation" class="active"><a href="#">Home</a></li>
+              <li role="presentation"><a href="#">Profile</a></li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <div class="row">
-        <a href="#" class="btn btn-success btn-sm">Añadir embudo nuevo</a>
-      </div>  
-      
-    </div>
-
-  </article>
-</section>
+    </article>
+  </section>
 </div>
